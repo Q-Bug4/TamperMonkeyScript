1a8c56527a9272a514dc4ec80d5918d155f69017
齐 <48374300+Q-Bug4@users.noreply.github.com>
2022-08-27 23:59:58 +0800

    Update RarbgBigPicture.js

--- RarbgBigPicture.js
+++ RarbgBigPicture.js
@@ -1,8 +1,7 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         RarbgBigPicture
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
 // @author       Q-Bug4
 // @match        https://proxyrarbg.org/torrent/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=proxyrarbg.org
@@ -13,7 +12,7 @@
     'use strict';
     let imgs = $("#description").find("img")
     for (let i=0;i<imgs.length;i++) {
-        let newSrc = imgs[0].src.replace(/22pixx.xyz\/os\//, "22pixx.xyz/o/")
+        let newSrc = imgs[i].src.replace(/22pixx.xyz\/os\//, "22pixx.xyz/o/")
         imgs.eq(i).attr("src", newSrc)
         imgs.eq(i).parent().attr("href", newSrc)
     }
